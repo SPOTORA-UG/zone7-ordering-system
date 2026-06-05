c9aae15370410ee462e514d439a3c7c950e9bfcc
SPOTORA-UG <spotoraug@gmail.com>
2026-06-05 10:15:04 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,19 @@
+import "./globals.css";
 
+export const metadata = {
+  title: "Zone 7 Ordering",
+  description: "QR Ordering by Spotora",
+};
+
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
