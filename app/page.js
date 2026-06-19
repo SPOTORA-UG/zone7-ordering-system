d9bd96ade321b13cb56ab0652cadca3687f2474d
SPOTORA-UG <spotoraug@gmail.com>
2026-06-19 11:00:38 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1885,8 +1885,6 @@ const sendOrder = () => {
 </button>
             </div>
       )}
-    </div>
-</section>
       </div>
 
       <section className="fixed bottom-0 left-0 right-0 bg-zinc-950 border-t border-yellow-600/40 p-4">
